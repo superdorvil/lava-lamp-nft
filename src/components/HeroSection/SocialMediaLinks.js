c2d2e0fdb737501b51862d6018430a113a7d72bf
dgabriel <dgabriel999@gmail.com>
2021-11-04 13:02:31 -0400

    fixed oepnsea link, lol

--- src/components/HeroSection/SocialMediaLinks.js
+++ src/components/HeroSection/SocialMediaLinks.js
@@ -9,7 +9,7 @@ function SocialMediaLinks() {
       <LinkButtonWrapper href="https://twitter.com/LavaLampsNFT">
         <Icon icon={ICONS.TWITTER} iconColor='#FFFFFF' />
       </LinkButtonWrapper>
-      <LinkButtonWrapper href="https://https://testnets.opensea.io/collection/lavalamp-qr0zzvimry">
+      <LinkButtonWrapper href="https://testnets.opensea.io/collection/lavalamp-qr0zzvimry">
         <Icon icon={ICONS.OPENSEA} iconColor='#FFFFFF' />
       </LinkButtonWrapper>
       <LinkButtonWrapper href="https://discord.gg/kHjfv5t2Ns">
