f82dc0423371260273ca780c698d019f3ebda0ba
dgabriel <dgabriel999@gmail.com>
2021-11-05 23:09:57 -0400

    smarter social media link t o opensea

--- src/components/HeroSection/SocialMediaLinks.js
+++ src/components/HeroSection/SocialMediaLinks.js
@@ -9,7 +9,7 @@ function SocialMediaLinks() {
       <LinkButtonWrapper href="https://twitter.com/LavaLampsNFT">
         <Icon icon={ICONS.TWITTER} iconColor='#FFFFFF' />
       </LinkButtonWrapper>
-      <LinkButtonWrapper href="https://testnets.opensea.io/collection/lavalamp-qr0zzvimry">
+      <LinkButtonWrapper href={`https://testnets.opensea.io/assets/${process.env.CONTRACT_ADDRESS}/0`}>
         <Icon icon={ICONS.OPENSEA} iconColor='#FFFFFF' />
       </LinkButtonWrapper>
       <LinkButtonWrapper href="https://discord.gg/kHjfv5t2Ns">
@@ -20,3 +20,5 @@ function SocialMediaLinks() {
 }
 
 export default SocialMediaLinks;
+
+//<LinkButtonWrapper href="https://testnets.opensea.io/collection/lavalamp-qr0zzvimry">
