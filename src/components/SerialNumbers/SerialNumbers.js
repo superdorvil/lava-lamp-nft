ccc76a43b0dc7de992f16139d2ea3fffd5da4cbb
dgabriel <dgabriel999@gmail.com>
2021-12-27 12:28:54 -0500

    Changed name of svg hoping it fixes build error

--- src/components/SerialNumbers/SerialNumbers.js
+++ src/components/SerialNumbers/SerialNumbers.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Container, Description, SerialImage} from './Elements';
-import serialNumbers from '../../images/SerialNumbers.svg';
+import serialNumbersSVG from '../../images/SerialNumbers.svg';
 
 function SerialNumbers() {
   return(
     <Container>
-      <SerialImage src={serialNumbers} />
+      <SerialImage src={serialNumbersSVG} />
       <Description>YOUR LAVA LAMP'S UNIQUE COLORS ARE TIED TOTHEIR SERIAL NUMBERS</Description>
     </Container>
   );
