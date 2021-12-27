285f68e62b9b49137ff7ec0a91b5d29a9c754522
dgabriel <dgabriel999@gmail.com>
2021-12-27 12:45:00 -0500

    fix plesase

--- src/components/SerialNumbers/SerialNumbers.js
+++ src/components/SerialNumbers/SerialNumbers.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Container, Description, SerialImage} from './Elements';
-import serialNumbersSVG from '../../images/SerialNumbers.svg';
+import serialNum from './../../images/SerialNumbersImage.svg';
 
 function SerialNumbers() {
   return(
     <Container>
-      <SerialImage src={serialNumbersSVG} />
+      <SerialImage src={serialNum} />
       <Description>YOUR LAVA LAMP'S UNIQUE COLORS ARE TIED TOTHEIR SERIAL NUMBERS</Description>
     </Container>
   );
