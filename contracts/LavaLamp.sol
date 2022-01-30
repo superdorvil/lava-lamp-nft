eeccd02d62edf60da9adb0a4fd13b6509b090a7e
dgabriel <dgabriel999@gmail.com>
2022-01-30 18:10:51 -0500

    updated contracts

--- contracts/LavaLamp.sol
+++ contracts/LavaLamp.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// truffle migrate --reset --network rinkeby
 contract LavaLamp is Ownable, ERC721 {
     using Strings for uint256;
     string private currentBaseURI;
@@ -13,11 +12,15 @@ contract LavaLamp is Ownable, ERC721 {
     uint256 private cost = 0.03 ether;
     uint256 private maxSupply = 7980;
     uint256 private maxMintAmount = 20;
+    address private lavaLampsEth = "0x4024b17b6fD2AC7a83bfE0c71970D23eE335BC40";
 
     uint256 public burnCount = 0;
     uint256 public lampsMinted = 0;
 
-    // string memory provenanceHash
+    // used to semi randomize the ids minted
+    uint256 lampCount = 0;
+    uint256 currentLampSet = 0;
+
     constructor() ERC721("LavaLamp", "LAVALAMP") {
         // "http://localhost:3000/token/"
         // setBaseURI("http://www.websitename.com/token/");
@@ -43,20 +46,85 @@ contract LavaLamp is Ownable, ERC721 {
             require(msg.value >= price);
 
         for (uint256 i = 1; i <= mintAmount; i++) {
-            _safeMint(msg.sender, lampsMinted);
+            uint256 tokenId = getId();
+            _safeMint(msg.sender, tokenId);
+            incrementId();
             lampsMinted++;
         }
 
-        // (bool hs, ) = payable(0x0).call{value: address(this).balance * 5 / 100}("");
-        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+        (bool success, ) = payable(lavaLampsEth).call{value: address(this).balance}("");
         require(success);
     }
 
-    /*function withdraw() public payable onlyOwner {
-      // (bool hs, ) = payable(0x0).call{value: address(this).balance * 5 / 100}("");
-      (bool success, ) = payable(owner()).call{value: address(this).balance}("");
-      require(success);
-    }*/
+    function getId() internal view returns(uint256) {
+        uint256 lampSetSize = 399;
+        uint256 randomLampBlock = randomizeLampBlock(currentLampSet);
+        uint256 id = randomLampBlock * lampSetSize + lampCount;
+
+        return id;
+    }
+
+    function incrementId() internal {
+        uint256 numOfLampBlocks = 19;
+
+        if (currentLampSet < numOfLampBlocks) {
+            currentLampSet = currentLampSet + 1;
+        } else {
+            currentLampSet = 0;
+            lampCount = lampCount + 1;
+        }
+    }
+
+    // Block has nothing to do with blockchain
+    // I just make groups of numbers and shuffle the order of ids minted
+    // If someone mints 20 lamps, they dont get 1-20
+    // Its not technically random, an actual random solution would be expensive
+    function randomizeLampBlock(uint256 lampBlock) internal pure returns(uint256) {
+        uint256 randomLampBlock = 0;
+
+        if (lampBlock == 0)
+            randomLampBlock = 3;
+        else if (lampBlock == 1)
+            randomLampBlock = 17;
+        else if (lampBlock == 2)
+            randomLampBlock = 10;
+        else if (lampBlock == 3)
+            randomLampBlock = 13;
+        else if (lampBlock == 4)
+            randomLampBlock = 18;
+        else if (lampBlock == 5)
+            randomLampBlock = 0;
+        else if (lampBlock == 6)
+            randomLampBlock = 4;
+        else if (lampBlock == 7)
+            randomLampBlock = 1;
+        else if (lampBlock == 8)
+            randomLampBlock = 6;
+        else if (lampBlock == 9)
+            randomLampBlock = 19;
+        else if (lampBlock == 10)
+            randomLampBlock = 8;
+        else if (lampBlock == 11)
+            randomLampBlock = 12;
+        else if (lampBlock == 12)
+            randomLampBlock = 2;
+        else if (lampBlock == 13)
+            randomLampBlock = 14;
+        else if (lampBlock == 14)
+            randomLampBlock = 11;
+        else if (lampBlock == 15)
+            randomLampBlock = 7;
+        else if (lampBlock == 16)
+            randomLampBlock = 15;
+        else if (lampBlock == 17)
+            randomLampBlock = 9;
+        else if (lampBlock == 18)
+            randomLampBlock = 5;
+        else if (lampBlock == 19)
+            randomLampBlock = 16;
+
+        return randomLampBlock;
+    }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
--- test/LavaLamp.test.js
+++ test/LavaLamp.test.js
@@ -1,14 +1,5 @@
-// burncount
-// totalSupply
-// provenance
-// mint
-// withdraw
-// tokenURI
-// burn
-// get tokens owned by
-//const { assert, expect } = require("chai");
 const {expectRevert} = require('@openzeppelin/test-helpers');
-
+const {idList} = require('./IDList')
 const tokensOwnedHelper = (tokenIds) => {
   const tokensOwned = [];
   tokenIds.forEach((owned, i) => {
@@ -19,6 +10,13 @@ const tokensOwnedHelper = (tokenIds) => {
 
   return tokensOwned;
 }
+function generateRandomNumber() {
+  const rand = Math.floor(Math.random() * 7980)
+
+  return rand
+};
+
+const test = true;
 
 const getWei = (weiBalance) => {
     let ethBalance = web3.utils.fromWei(weiBalance, 'ether');
@@ -193,4 +191,106 @@ contract('LavaLamp', (accounts) => {
       //  await contract2.mint(21, { from: minter1, value: price * 10 });
     })*/
   })
+
+  /*
+  describe('Getting specific tokens', async () => {
+    it("gets supply", async () => {
+      let totalSupply = await contract.totalSupply()
+      console.log('total supply = ' + totalSupply)
+      assert.equal(totalSupply, 3)
+        //let owner = await contract.ownerOf(1197)
+        // lavaLampsETH
+        // minter1
+      await contract.claim(10, { from: minter1, value: price * 10 })
+      const allTokens = await contract.getAllTokenMetadata()
+      allTokens.forEach((meta, i) => {
+        console.log(metaDataToID(meta))
+      });
+
+      totalSupply = await contract.totalSupply()
+      assert.equal(totalSupply, 13)
+
+      if (test) {
+        const rand0 = generateRandomNumber()
+        const rand1 = generateRandomNumber()
+        const rand2 = generateRandomNumber()
+        const rand3 = generateRandomNumber()
+        const rand4 = generateRandomNumber()
+        const rand5 = generateRandomNumber()
+        const rand6 = generateRandomNumber()
+        const rand7 = generateRandomNumber()
+        const rand8 = generateRandomNumber()
+        const rand9 = generateRandomNumber()
+        const rand10 = generateRandomNumber()
+        const rand11 = generateRandomNumber()
+        const rand12 = generateRandomNumber()
+        const rand13 = generateRandomNumber()
+        const rand14 = generateRandomNumber()
+
+        const token0 = await contract.tokenByIndex(0)
+        const token1 = await contract.tokenByIndex(1)
+        const token2 = await contract.tokenByIndex(2)
+        const randToken0 = await contract.tokenByIndex(rand0)
+        const randToken1 = await contract.tokenByIndex(rand1)
+        const randToken2 = await contract.tokenByIndex(rand2)
+        const randToken3 = await contract.tokenByIndex(rand3)
+        const randToken4 = await contract.tokenByIndex(rand4)
+        const randToken5 = await contract.tokenByIndex(rand5)
+        const randToken6 = await contract.tokenByIndex(rand6)
+        const randToken7 = await contract.tokenByIndex(rand7)
+        const randToken8 = await contract.tokenByIndex(rand8)
+        const randToken9 = await contract.tokenByIndex(rand9)
+        const randToken10 = await contract.tokenByIndex(rand10)
+        const randToken11 = await contract.tokenByIndex(rand11)
+        const randToken12 = await contract.tokenByIndex(rand12)
+        const randToken13 = await contract.tokenByIndex(rand13)
+        const randToken14 = await contract.tokenByIndex(rand14)
+        const token7979 = await contract.tokenByIndex(7979)
+
+        assert.equal(token0, idList[0])
+        assert.equal(token1, idList[1])
+        assert.equal(token2, idList[2])
+        assert.equal(token2, idList[2])
+        assert.equal(randToken0, idList[rand0])
+        assert.equal(randToken1, idList[rand1])
+        assert.equal(randToken2, idList[rand2])
+        assert.equal(randToken3, idList[rand3])
+        assert.equal(randToken4, idList[rand4])
+        assert.equal(randToken5, idList[rand5])
+        assert.equal(randToken6, idList[rand6])
+        assert.equal(randToken7, idList[rand7])
+        assert.equal(randToken8, idList[rand8])
+        assert.equal(randToken9, idList[rand9])
+        assert.equal(randToken10, idList[rand10])
+        assert.equal(randToken11, idList[rand11])
+        assert.equal(randToken12, idList[rand12])
+        assert.equal(randToken13, idList[rand13])
+        assert.equal(randToken14, idList[rand14])
+        assert.equal(token7979, idList[7979])
+
+        const lavaLampsETHToken0 = await contract.tokenOfOwnerByIndex(lavaLampsETH, 0)
+        const lavaLampsETHToken1 = await contract.tokenOfOwnerByIndex(lavaLampsETH, 1)
+        const lavaLampsETHToken2 = await contract.tokenOfOwnerByIndex(lavaLampsETH, 2)
+        const minter1Token0 = await contract.tokenOfOwnerByIndex(minter1, 0)
+        const minter1Token1 = await contract.tokenOfOwnerByIndex(minter1, 1)
+        const minter1Token2 = await contract.tokenOfOwnerByIndex(minter1, 2)
+        const minter1Token3 = await contract.tokenOfOwnerByIndex(minter1, 3)
+
+        console.log('lavaLampsETH tokens')
+        console.log(lavaLampsETHToken0)
+        console.log(lavaLampsETHToken1)
+        console.log(lavaLampsETHToken2)
+        console.log('minter1 tokens')
+        console.log(minter1Token0)
+        console.log(minter1Token1)
+        console.log(minter1Token2)
+        console.log(minter1Token3)
+      } else {
+        console.log('Just a heads up, change test variable to  true')
+        console.log('Turned it false, because calling await a bunch of times takes too long')
+        console.log('false skips some test and thus makes it go faster')
+      }
+    })
+  })
+  */
 })
